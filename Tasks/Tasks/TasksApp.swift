7a2a920be8aa2cafdb40b973e4b4c8c92406e2a8
Trey Thompson <treythompson152@gmail.com>
2024-04-22 15:34:29 -0600

    init

---
+++ Tasks/Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Trey Thompson on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
